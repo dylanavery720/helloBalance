1a494404d320f94feb0321bea79251ea60fc0a5a
Dylan Avery <fossilfuelmusic@gmail.com>
2017-05-13 08:39:10 -0600

    Start standard token

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,20 +1,56 @@
 pragma solidity ^0.4.4;
 
-contract HelloWorld { 
-    uint public balance;
-    
-    //constructor
-    //runs once upon contract creation
+
+//web3.eth.accounts
+//account2 = web3.eth.accounts[1]
+
+contract HelloWorld {
+   //hash table
+    address public owner;
+    mapping (address => uint) balances;
+
     function HelloWorld() {
-      balance = 1000;    
+      //msg.sender is the acct address calling contract
+      owner = msg.sender;
+      balances[owner] = 1000;
     }
 
-    function Deposit(uint _value) returns(uint _newValue) {
-      // ...
-      balance += _value;
-      return balance; 
+    function transfer(address _to, uint _value) returns (bool success) {
+      if (balances[msg.sender] < _value) {
+        return false;
+      }
+      
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      return true;
     }
+  
+    function getBalance(address _user) constant returns (uint _balance) {
+      return balances[_user];
+    }
+
+    //call getBalance .. helloWorld.then(instance => instance.getBalance.call(account2)).then(balance => console.log(balance))
 }
+
+
+
+
+
+// contract HelloWorld { 
+//     uint public balance;
+    
+//     //constructor
+//     //runs once upon contract creation
+//     function HelloWorld() {
+//       balance = 1000;    
+//     }
+
+//     function Deposit(uint _value) returns(uint _newValue) {
+//       // ...
+//       balance += _value;
+//       return balance; 
+//     }
+// }
 
 //new truffle call .. 
 //HelloWorld.deployed()
